14c586012b9b7b91b4b7908dbeb76cb918d94f34
Franck Magnan <fmagnan@gmail.com>
2013-03-26 09:57:38 +0100

    finishing challenge

--- cubesCutter/cubesCutter.php
+++ cubesCutter/cubesCutter.php
@@ -1,47 +1,76 @@
 <?php
 
-function getVolume($piece) {
-	return $piece['length']*$piece['width']*$piece['height'];
+/**
+ * gcd functions picked up from
+ * http://www.php.net/manual/en/function.gmp-gcd.php
+ */
+
+/*
+ * function gcd()
+ *
+ * returns greatest common divisor
+ * between two numbers
+ * tested against gmp_gcd()
+ */
+function gcd($a, $b)
+{
+    if ($a == 0 || $b == 0)
+        return abs( max(abs($a), abs($b)) );
+
+    $r = $a % $b;
+    return ($r != 0) ?
+        gcd($b, $r) :
+        abs($b);
+}
+
+/*
+ * function gcd_array()
+ *
+ * gets greatest common divisor among
+ * an array of numbers
+ */
+function gcd_array($array, $a = 0)
+{
+    $b = array_pop($array);
+    return ($b === null) ?
+        (int)$a :
+        gcd_array($array, gcd($a, $b));
+}
+
+/**
+ * @param array parallelepipedic $piece of wood
+ * @return int volume
+ */
+
+function getPieceVolume(array $piece) {
+	return (int) $piece[0]*$piece[1]*$piece[2];
 }
 
 function main()
 {
-	$stdin = fopen(__DIR__.'/data/variousPieces.txt', 'r');
-	$stdout = fopen(__DIR__.'/out.log', 'w');
-	
+    $stdin = fopen('php://stdin', 'r');
+    $stdout = fopen('php://stdout', 'w');
+
 	$uselessFirstLine=fgets($stdin);
+    unset($uselessFirstLine);
 
 	$totalVolume=0;
-	$minimumCubeDimension=100;
-	$pieces=array();	
+	$minimumCubeSide=100;
 
 	while(false!==($line=fgets($stdin)))
 	{
-		$dimensions=explode(';',$line);
-		$woodPiece=array('length'=>(int)$dimensions[0],'width'=>(int)$dimensions[1],'height'=>(int)$dimensions[2]);
-		$pieces[]=$woodPiece;
+        $woodPiece=array();
+		foreach(explode(';',$line) as $dimension){
+            $woodPiece[]=(int)$dimension;
+        }
 
-		$totalVolume+=getVolume($woodPiece);
-		$minimumCubeDimension=min(min($woodPiece),$minimumCubeDimension);
+        $minimumCubeSide=min($minimumCubeSide,gcd_array($woodPiece));
+
+		$totalVolume+=getPieceVolume($woodPiece);
 	}
 
-	foreach($pieces as $piece)
-	{
-		$remain=$totalVolume%getVolume($piece);
-		if(0===$remain)
-		{
-			continue;
-		}
-		
-		
-	}
+	fputs($stdout, $minimumCubeSide.';'.$totalVolume / pow($minimumCubeSide,3));
 
-	fputs($stdout, $minimumCubeDimension.';'.$totalVolume / pow($minimumCubeDimension,3));
-
-echo 'volume total = ' . $totalVolume . "\n";
-echo 'min cote = ' . $minimumCubeDimension . "\n";
-echo 'nb cubes = ' . $totalVolume / pow($minimumCubeDimension,3) . "\n";
-	
 	fclose($stdout);
 	fclose($stdin);
 }
